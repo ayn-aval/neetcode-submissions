5ea6fbc62e1d97075298bf9c7d4c3b9bb634607a
Lavanya L Nair <lavanya.nair.bce22@itbhu.ac.in>
2026-06-16 23:37:54 +0530

    Add: multiply-strings - submission-0

---
+++ Data Structures & Algorithms/multiply-strings/submission-0.py
@@ -0,0 +1,7 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        s= ""
+        num1_int = int(num1)
+        num2_int = int(num2)
+        return str((num1_int * num2_int))
+    

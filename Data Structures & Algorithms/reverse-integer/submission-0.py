172dad1276b5c9e4d6c8882c87e9bfa43f4b58a0
Lavanya L Nair <lavanya.nair.bce22@itbhu.ac.in>
2026-07-31 08:38:54 +0530

    Add: reverse-integer - submission-0

---
+++ Data Structures & Algorithms/reverse-integer/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        org = x
+        x = abs(x)
+        res = int(str(x)[::-1])
+        if org < 0:
+            res *=-1
+        if res < -2**31 or res > ((2**31)-1):
+            return 0
+        return res
+        
+        
+        
+
+    

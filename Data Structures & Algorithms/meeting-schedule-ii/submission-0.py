be20a2a18a86793b20994b5639ca3cbaf81f8e9c
Lavanya L Nair <lavanya.nair.bce22@itbhu.ac.in>
2026-08-12 22:52:09 +0530

    Add: meeting-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-0.py
@@ -0,0 +1,20 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        starts = sorted(i.start for i in intervals)
+        ends = sorted(i.end for i in intervals)
+        rooms = end_pointer = 0
+        for s in starts:
+            if s< ends[end_pointer]:
+                rooms += 1
+            else:
+                end_pointer += 1
+        return rooms
+        

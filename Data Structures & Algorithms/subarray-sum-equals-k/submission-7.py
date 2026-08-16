8aa9bd27f03f1110c1c749312b1cfc1614e1b038
Lavanya L Nair <lavanya.nair.bce22@itbhu.ac.in>
2026-08-16 14:49:24 +0530

    Add: subarray-sum-equals-k - submission-7

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-7.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        count = 0
+        total = 0
+        d = {0:1}
+        
+        for num in nums:
+            total += num
+
+            if total-k in d:
+                count += d[total-k]
+            if total in d:
+                d[total] += 1
+            else:
+                d[total] = 1
+        return count
